58b189564da540fffea048658d84e0402d743301
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-10-10 14:03:52 +0900

    2. 타일이동  및 캐릭터 점프구현
    타일 리소스를 불러와 화면에 띄워 이동시키고 캐릭터 리소스를 불러와 왼쪽마우스 클릭시 점프하도록 구현.  점프 시 사용 함수는 sin함수를 이용

--- 2DGP - geometry dash/background move.py
+++ 2DGP - geometry dash/background move.py
@@ -1,17 +1,38 @@
 from pico2d import*
+import  math
 open_canvas(1020,510)
 
+TILESIZE = 100
+TILECNT = 100
+CHARACTERSIZE = 60
 #init
 
 background_1 = load_image('background1.png')
 background_2 = load_image('background1.png')
 background_3 = load_image('background1.png')
+basic_tile = load_image('basic_tile.png')
+character = load_image('character.png')
 
 background_1_pivotX = 255
 background_2_pivotX = 765
 background_3_pivotX = 1275
 backgroundPivotY = 255
 speed = 1
+character_jump =False
+
+def handle_events():
+    global running
+    global character_jump
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDL_ESCAPE:
+                # 이부분은 이후 일시정지 되도록 바꾼다 -> 추가구현 내용
+                running = False
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            character_jump = True
 
 def BackgroundMove():
     global background_1_pivotX
@@ -38,16 +59,57 @@ def UpSpeed():
     global speed
     speed += 0.0001
 
+def InitMap():
+    #초기 단계이므로 일단 타일만 깐다
+    global basic_tiles_x, basic_tiles_y # tiles pivot value
+    basic_tiles_x = [n*100 + TILESIZE/2 for n in range(0,TILECNT)]
+    basic_tiles_y = [50 for n in range(0,TILECNT)]
 
-while(True):
+def TilesMove():
+    for i in range(0,TILECNT):
+        basic_tiles_x[i] -= speed
+
+def DrawTiles():
+    for i in range(0,TILECNT):
+        if(basic_tiles_x[i]>=-50 and basic_tiles_x[i]<=1070):
+            basic_tile.draw(basic_tiles_x[i],basic_tiles_y[i])
+
+def MoveCharacter():
+    global character_x, character_y
+    global degree
+    if(character_jump == True):
+        character_y += math.sin(degree/360 *2 *math.pi) * 10
+    degree+=5
+
+
+
+
+running = True
+basic_tiles_x = [0 for n in range(0, TILECNT)]
+basic_tiles_y = [50 for n in range(0, TILECNT)]
+character_x, character_y = 130, 130
+
+degree = 0
+
+InitMap()
+while(running):
+    handle_events()
     #update
     BackgroundMove()
     UpSpeed()
-    
-    #render
+    TilesMove()
+    MoveCharacter()
+    if (character_y<130):
+        degree = 0
+        character_y = 130
+        character_jump = False
+    #draw
     clear_canvas()
     BackgroundDraw(background_1_pivotX,background_2_pivotX,background_3_pivotX,backgroundPivotY)
+    DrawTiles()
+    character.clip_draw(0,0,117,118,character_x,character_y,CHARACTERSIZE,CHARACTERSIZE)
     update_canvas()
+
 
     delay(0.01)
 
