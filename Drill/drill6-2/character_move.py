c855a6195895699ef30d36a5d468cd7c531e4941
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-10-11 11:54:58 +0900

    drill 6-2 커밋5

--- Drill/drill6-2/character_move.py
+++ Drill/drill6-2/character_move.py
@@ -18,12 +18,11 @@ while True:
         character_x = (2 * t ** 2 - 3 * t + 1) * vertex_x[0] + (-4 * t ** 2 + 4 * t) * vertex_x[1] + (2 * t ** 2 - t) * vertex_x[2]
         character_y = (2 * t ** 2 - 3 * t + 1) * vertex_y[0] + (-4 * t ** 2 + 4 * t) * vertex_y[1] + (2 * t ** 2 - t) * vertex_y[2]
         background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        if (vertex_x[n + 1] < vertex_x[n]):
+        if (vertex_x[n + 1] > vertex_x[n]):
             character.clip_draw(character_frame_x * 100, 100, 100, 100, character_x, character_y)
         else:
             character.clip_draw(character_frame_x * 100, 0, 100, 100, character_x, character_y)
         update_canvas()
-        handle_events()
         character_frame_x = (character_frame_x + 1) % 8
         delay(0.03)
     n=(n+1)%10
@@ -31,33 +30,31 @@ while True:
     for n in range(1,7):
         for i in range(0,100):
             t = i / 100
-            character_x = ((-t ** 3 + 2 * t ** 2 - t) * vertex_x[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_x[n] + (-3 * t ** 3 + 4 * t ** 2 + t) * vertex_x[n+1] + (t ** 3 - t ** 2) * character_x[n+2]) / 2
-            character_y = ((-t ** 3 + 2 * t ** 2 - t) * vertex_y[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_y[n] + (-3 * t ** 3 + 4 * t ** 2 + t) * vertex_y[n+1] + (t ** 3 - t ** 2) * character_y[n+2]) / 2
+            character_x = ((-t ** 3 + 2 * t ** 2 - t) * vertex_x[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_x[n] + (-3 * t ** 3 + 4 * t ** 2 + t) * vertex_x[n+1] + (t ** 3 - t ** 2) * vertex_x[n+2]) / 2
+            character_y = ((-t ** 3 + 2 * t ** 2 - t) * vertex_y[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_y[n] + (-3 * t ** 3 + 4 * t ** 2 + t) * vertex_y[n+1] + (t ** 3 - t ** 2) * vertex_y[n+2]) / 2
             background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-            if (vertex_x[n + 1] < vertex_x[n]):
+            if (vertex_x[n + 1] > vertex_x[n]):
                 character.clip_draw(character_frame_x * 100, 100, 100, 100, character_x, character_y)
             else:
                 character.clip_draw(character_frame_x * 100, 0, 100, 100, character_x, character_y)
             update_canvas()
-            handle_events()
             character_frame_x = (character_frame_x + 1) % 8
             delay(0.03)
         n = (n + 1) % 10
 
-    for n in range(7,9):
-        for n in range(0,100):
+    for n in range(7,10):
+        for i in range(0,100):
             t = i / 100
             character_x = ((-t ** 3 + 2 * t ** 2 - t) * vertex_x[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_x[n] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * vertex_x[(n+1)%10] + (t ** 3 - t ** 2) * vertex_x[(n+2)%10]) / 2
+                    -3 * t ** 3 + 4 * t ** 2 + t) * vertex_x[((n+1)%10)] + (t ** 3 - t ** 2) * vertex_x[((n+2)%10)]) / 2
             character_y = ((-t ** 3 + 2 * t ** 2 - t) * vertex_y[n-1] + (3 * t ** 3 - 5 * t ** 2 + 2) * vertex_y[n] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * vertex_y[(n+1)%10] + (t ** 3 - t ** 2) * vertex_y[(n+2)%10]) / 2
+                    -3 * t ** 3 + 4 * t ** 2 + t) * vertex_y[((n+1)%10)] + (t ** 3 - t ** 2) * vertex_y[((n+2)%10)]) / 2
             background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-            if (vertex_x[(n+1)%10] < vertex_x[n]):
+            if (vertex_x[(n+1)%10] > vertex_x[n]):
                 character.clip_draw(character_frame_x * 100, 100, 100, 100, character_x, character_y)
             else:
                 character.clip_draw(character_frame_x * 100, 0, 100, 100, character_x, character_y)
             update_canvas()
-            handle_events()
             character_frame_x = (character_frame_x + 1) % 8
             delay(0.03)
         n = (n + 1) % 10
