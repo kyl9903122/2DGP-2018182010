d5b7eca3e37d51bf6b52356d0d128ef046f8eb9d
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-10-11 11:15:58 +0900

    drill6-1 커밋 1

--- Drill/drill6-1/curve.py
+++ Drill/drill6-1/curve.py
@@ -97,7 +97,13 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
-    #draw
+    #draw p4-p1
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p1[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p0[1])/2
+        draw_point((x, y))
+    draw_point(p3)
 
 
 
