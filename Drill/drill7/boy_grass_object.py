93c11680c4676f548622a36fadac0d2f6bee9ef9
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-10-18 12:06:09 +0900

    drill7 commit4

--- Drill/drill7/boy_grass_object.py
+++ Drill/drill7/boy_grass_object.py
@@ -31,7 +31,7 @@ class Ball:
             self.x, self.y = random.randint(100, 600), 490
         else:
             self.x, self.y = random.randint(100, 600), 480
-        self.speed = random.randint(7, 15)
+        self.speed = random.randint(3, 10)
         if(self.size == 0):
             self.image = load_image('ball21x21.png')
         else:
