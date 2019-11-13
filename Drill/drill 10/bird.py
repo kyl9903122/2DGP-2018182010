bc30f4fa0e7e105a69f0677c62be4c65cbb3241c
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-11-13 23:21:29 +0900

    드릴 10 완료
    2018182010 김유림
    커밋횟수: 4
    https://youtu.be/YfnYV-wAS1k

--- Drill/drill 10/bird.py
+++ Drill/drill 10/bird.py
@@ -4,21 +4,21 @@ from pico2d import *
 import game_world
 
 # bird speed
-PIXEL_PER_METER = (1 / 10.0)
-RUN_SPEED_KMPH = 15.0
+PIXEL_PER_METER = (1 / 0.3)
+RUN_SPEED_KMPH = 30.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # bird frame
-TIME_PER_ACTION = 1.0
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 0.5 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
 
 
 class BIRD:
     def __init__(self):
-        self.x, self.y = 900 // 2, 200
+        self.x, self.y = 900 // 2, 300
         self.image = load_image('bird_animation.png')
         self.dir = 1
         self.velocity = 0
@@ -40,14 +40,15 @@ class BIRD:
             self.dir = -1
             self.velocity = 0
 
+
     def draw(self):
         print("enter draw")
         if self.dir == 1:
-            self.image.clip_draw((int(self.frame) % 5) * 182, (2-int(self.frame // 5)) * 164, 183, 164, self.x, self.y, 180,
+            self.image.clip_draw((int(self.frame) % 5) * 182, (2-int(self.frame // 5)) * 165, 183, 165, self.x, self.y, 180,
                                  160)
             print(int(self.frame) % 5, int(self.frame // 5))
         else:
-            self.image.clip_composite_draw(int(self.frame % 5) * 182, (2-int(self.frame / 5) )* 164, 183, 164, 0, 'h',
+            self.image.clip_composite_draw(int(self.frame % 5) * 182, (2-int(self.frame / 5) )* 165, 183, 165, 0, 'h',
                                            self.x, self.y, 180, 160)
             print(int(self.frame) % 5, int(self.frame // 5))
 
